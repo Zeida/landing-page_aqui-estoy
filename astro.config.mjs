b3834b685408c88d13945929331cdd2b83da4158
Zeida <zeidaromen@outlook.es>
2025-01-15 20:02:35 +0000

    Conf for netlify on astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+/* For gh pages
 export default defineConfig({
   site: 'https://zeida.github.io/landing-page_aqui-estoy', 
   base: '/landing-page_aqui-estoy/',
+  output: 'static'
+});
+*/
+
+export default defineConfig({
+  site: 'https://aqui-estoy.netlify.app', 
+  base: '/',
   output: 'static'
 });
